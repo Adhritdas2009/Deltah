8b2533096cee81cdc638d8cb75ef7cc0227ccaad
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2022-02-23 21:47:43 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location="index.html"
+}
+
+function info(){
+    window.location="info.html"
+}
+
+function contact(){
+    window.location="contact.html"
+}
+
+function talk(){
+    window.location="talk.html"
+}
